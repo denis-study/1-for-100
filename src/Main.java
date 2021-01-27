aa4fc86d3f25c10fc7b8f01aed711fbbd773dbce
Denis T <dengertd@gmail.com>
2021-01-27 13:07:59 +0300

    modify Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         int bonus = 1;
-        int replenishment = 3100;
+        int replenishment = 1505;
         int balance = 200;
         int bonuses = replenishment / 100 * bonus;
         int amount = bonuses + balance + replenishment;
 
+        if (replenishment > 1000) {
+            bonuses = replenishment / 100 * bonus;
+        } else {
+            bonuses = 0;
+        }
 
         System.out.println(bonuses);
     }
